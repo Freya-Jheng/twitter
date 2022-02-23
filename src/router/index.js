174bdc5db4ff14f60cc9821989a19543230a49c1
huanmingchang <huanming1988@gmail.com>
2022-02-23 20:46:30 +0800

    feat: 新增 home 路由 feat: 建立 home 各元件的排版

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import NotFound from '../views/NotFound.vue'
 import SignIn from '../views/SignIn.vue'
+import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -10,6 +11,11 @@ const routes = [
     path: '/',
     name: 'root',
     redirect: '/signin',
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: Home,
   },
   {
     path: '/signin',
---
+++ src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">
+    <Navbar />
+    <div class="tweets"></div>
+    <div class="popular-users">Popular users</div>
+  </div>
+</template>
+
+<script>
+import Navbar from './../components/Navbar'
+
+export default {
+  name: 'Home',
+  components: {
+    Navbar,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.home {
+  display: grid;
+  grid-template-columns: 1fr 600px 1.22fr;
+  grid-template-rows: auto;
+  height: 100vh;
+  .navbar {
+    outline: 1px solid red;
+  }
+
+  .tweets {
+    border-left: 1px solid var(--border-and-divider);
+    border-right: 1px solid var(--border-and-divider);
+    height: 100%;
+    background: lightgrey;
+  }
+
+  .popular-users {
+    background: grey;
+    height: 100%;
+  }
+}
+</style>
