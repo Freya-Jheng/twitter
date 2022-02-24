6d92187ec3da8fb6a8aa5f5a8a0b977cb2481bbd
Freya Jheng <freyajheng@gmail.com>
2022-02-24 19:39:27 +0800

    feat

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,17 +1,11 @@
 <template>
   <div>
-    <Navbar />
     <h1>404 Not Found</h1>
   </div>
 </template>
 
 <script>
-import Navbar from './../components/Navbar.vue'
-
 export default {
   name: 'NotFound',
-  components: {
-    Navbar,
-  }
 }
 </script>
